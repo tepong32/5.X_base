69d84cf3411597a432bc483b4ae472e4df05b2e8
teppy <tepong32@gmail.com>
2024-09-03 13:19:33 +0800

    added forms from dj2.1.5

---
+++ Project5.x/home/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+'''
+	These forms are the ones the users will be using when registering or updating their account info.
+	They will be specifically added to their respective views, in our case:
+	
+	user.views.register > UserRegisterForm
+	user.views.profileEditView > UserUpdateForm
+'''
+
+class UserRegisterForm(UserCreationForm):
+	email = forms.EmailField() # to make sure they will be using a valid email address format
+
+	class Meta:
+		model = User 	# the mode that is going to be affected is the User model
+		fields = ["__all__"]
+
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField() # to make sure they will be using a valid email address format
+
+	class Meta:
+		model = User 	# the model that is going to be affected is the User model, 
+		# fields = ["email", "first_name", "middle_name", "last_name"]
+		fields = ["__all__"]
+		exclude = ["username"]
+
